ef4d0591fc3cbfe2b5b0bf0c773759df05bd0534
Abram Young <young@physics.arizona.edu>
2026-06-09 15:01:49 -0700

    reduce synth output to 6dBm

--- utils/HP8657B_syn.py
+++ utils/HP8657B_syn.py
@@ -14,7 +14,7 @@ ser.write("++addr 7\n".encode())   #address 14
 ser.write("++auto 0\n".encode())   #do not auto-read
 ser.write("++eos 3\n".encode())    #end of string CR+LF
 
-ser.write("AP +13.0 DM\n".encode())
+ser.write("AP +6.0 DM\n".encode())
 time.sleep(.5)
 cmd="FR %.6f MZ\n" % float(sys.argv[1])
 ser.write(cmd.encode())
